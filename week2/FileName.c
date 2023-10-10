6d9ba15022f420cfbcf25c754f4193fd7172d469
Cameron MCCARTHY <m017996j@student.staffs.ac.uk>
2023-10-10 15:12:50 +0100

    activity 6

--- week2/FileName.c
+++ week2/FileName.c
@@ -4,6 +4,9 @@
 
 void alphabet()
 {
+	/*prints all the letters of the alphabet
+	A=65 and a=A+32
+	Ascii indexes(97)*/
 	char i = 65;
 	for (; i <= 90; ++i)
 	{
@@ -11,9 +14,29 @@ void alphabet()
 	}
 }
 
+void activity6()
+{
+	/* bitwise */
+	/* integers in binary using "0b" */
+	int A = 0b0100;
+	int B = 0b0101;
+
+	/* A AND B */
+	printf("A AND B: ");
+	printf("%i  \r\n\n", A & B);
+
+	/* A OR B */
+	printf("A OR B: ");
+	printf("%i  \r\n\n", A | B);
+
+	/* A XOR B*/
+	printf("A XOR B: ");
+	printf("%i  \r\n\n", A ^ B);
+}
+
 int main()
 {
-	alphabet();
+	activity6();
 
 	return 0;
 }
