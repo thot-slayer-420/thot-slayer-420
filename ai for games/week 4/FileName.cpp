106d64c49c509a95f92353b53d259455c53cf5ab
Cameron MCCARTHY <m017996j@student.staffs.ac.uk>
2023-10-24 10:06:08 +0100

    Starting task 1

---
+++ ai for games/week 4/FileName.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+
+/*task1*/
+
+class search_array {
+private:
+	int array[10];
+
+public: 
+	search_array() { read(); }
+
+	void read()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			cout << "\nEnter a number:";
+			cin >> array[i];
+		}
+	}
+
+	void print() 
+	{
+		cout << "All the items in the list:\n";
+		for (int i = 0; i < 10; i++)
+		{
+			cout << array[i] << " ";
+		}
+	}
+
+	void bubble()
+	{
+		bool sorted = false;
+		int items_need_sorting = 10;
+		while (!sorted)
+		{
+			bool items_swapped = false;
+			for (int i = 0; i < items_need_sorting - 1; i++)
+			{
+				int temp;
+				if (array[i] > array[i + 1])
+				{
+					temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					items_swapped = true;
+				}
+			}
+			items_need_sorting--;
+			if (!items_swapped)
+			{
+				sorted = true;
+			}
+		}
+	}
+};
+
+
+int main() {
+
+
+
+
+
+	return 0;
+}
+
+
