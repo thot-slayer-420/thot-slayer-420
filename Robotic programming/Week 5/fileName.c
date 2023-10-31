de465628b6b9c4c32ae304bd73b4d90ece5caac6
Cameron MCCARTHY <m017996j@student.staffs.ac.uk>
2023-10-31 15:27:10 +0000

    starting the challenge

---
+++ Robotic programming/Week 5/fileName.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>
+#include <stdio.h>
+
+void clear_buffer(int user_input)
+{
+	if (user_input != 10)
+			{
+				int clear_buffer = getchar();
+				while (clear_buffer != 10 )
+				{
+					clear_buffer = getchar();
+				}
+			}
+}
+
+void challenge_task()
+{
+	float target_angle;
+	int user_input;
+	int want_to_exit = 0;
+	while (!want_to_exit)
+	{
+		printf("Give me the target agnle of the servo between -90.0 and 90.0: ");
+		user_input = getchar();
+		printf("%c", user_input);
+		clear_buffer(user_input);
+
+		int valid_input = 0;
+		printf("\n\rDo you want to exit?");
+		do {
+			user_input = getchar();
+			printf("%d", user_input);
+			clear_buffer(user_input);
+			switch (user_input)
+			{
+			case 89 | 121:
+				want_to_exit = 1;
+				valid_input = 1;
+				break;
+			case 78 | 110:
+				valid_input = 1;
+				break;
+			default:
+				printf("Invalid input%c",10);
+				break;
+			}
+		} while (valid_input = 0);
+
+	}
+}
+
+
+
+int main()
+{
+	challenge_task();
+
+
+
+
+	return 0;
+}
