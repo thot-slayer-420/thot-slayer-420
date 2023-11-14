38a009c8ae798eee54d319a77b6d1f001b015db5
Cameron MCCARTHY <m017996j@student.staffs.ac.uk>
2023-11-14 14:50:53 +0000

    graph libray v1

--- libraries/c++/graph/graph/graph.cpp
+++ libraries/c++/graph/graph/graph.cpp
@@ -4,47 +4,59 @@
 #include <iostream>
 using namespace std;
 
-class graph
-{
-public:
-    graph() { cout << "Graph created" << endl; }
-    void add_node(int id) { node new_node(id); nodes.push_back(new_node); cout << "Node " << id << "is created into the graph." << endl; }
-    void add_node(int id, int depth) { node new_node(id, depth); nodes.push_back(new_node); cout << "Node " << id << " is created at depth " << depth << endl; }
-    void add_edge(node& parent, node& child) { parent.add_edge(child); cout << "Parent node, " << parent.get_id() << ", has child node, " << child.get_id() << endl; }
-    node get_node(int search_id) {
-        for (int i = 0; i < nodes.size(); ++i) {
-            if (search_id == nodes[i].get_id()) { cout << "node " << search_id << " returned" << endl; return nodes[i]; }
+namespace graph_lib {
+    class node
+    {
+    public:
+        node(int new_id) { id = new_id; }
+        node(int new_id, int new_depth) { id = new_id; depth = new_depth; }
+
+        void add_edge(node& edge) { edges.push_back(edge); cout << "pointer from node " << id << " to " << edge.get_id() << " added" << endl; }
+        int get_id() { cout << "Node: " << id << " id returned" << endl; return id; }
+        int get_depth() { cout << "Node: " << depth << " depth returned" << endl; return depth; }
+        void set_id(int new_id) { id = new_id; cout << "id " << id << "set" << endl; }
+        void set_depth(int new_depth) { depth = new_depth; cout << "depth" << depth << " set" << endl; }
+
+
+    private:
+        vector<node> edges;
+        int id;
+        int depth;
+
+
+    };
+
+
+    class graph
+    {
+    public:
+        graph() { cout << "Graph created" << endl; }
+        void add_node(int id) { node new_node(id); nodes.push_back(new_node); cout << "Node " << id << " is created into the graph." << endl; }
+        void add_node(int id, int depth) { node new_node(id, depth); nodes.push_back(new_node); cout << "Node " << id << " is created at depth " << depth << endl; }
+        void add_edge(node parent, node child) { parent.add_edge(child); cout << "Parent node, " << parent.get_id() << ", has child node, " << child.get_id() << endl; }
+        node* get_node(int search_id) {
+            for (int i = 0; i < nodes.size(); ++i) {
+                if (search_id == nodes[i].get_id()) { cout << "node " << search_id << " returned" << endl; return &nodes[i]; }
+            }
+            cout << "Node " << search_id << " not found" << endl;
+            return 0;
         }
-        cout << "Node " << search_id << " not found" << endl;
-        return 0;
-    }
-private:
-    vector<node> nodes;
-};
+    private:
+        vector<node> nodes;
+    };
 
-class node
-{
-public:
-    node(int new_id) { id = new_id; }
-    node(int new_id, int new_depth) { id = new_id; depth = new_depth; }
+}
 
-    void add_edge(node& edge) { edges.push_back(edge); cout << "pointer from node " << id << " to " << edge.get_id() << " added" << endl; }
-    int get_id() { cout<< "Node: "<< id << " returned" << endl; return id; }
-    void set_id(int new_id) { id = new_id; cout << "id " << id << "set" << endl; }
-    
-
-private:
-    vector<node> edges;
-    int id;
-    int depth;
-
-   
-};
+using namespace graph_lib;
 
 int main()
 {
     graph x;
-    x.add_node(0);
+    for (int i = 0; i < 10; ++i)
+    {
+        x.add_node(i);
+        if (i > 0) { x.add_edge(*x.get_node(i - 1) , *x.get_node(i)); }
+    }
 
 }
 
